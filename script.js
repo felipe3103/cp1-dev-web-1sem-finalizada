d283ce3ec14bf93251a2560d48b57da679bba9a9
Felipe <felipenafe@gmail.com>
2024-03-27 13:03:40 -0300

    Felipe Fernandes https://github.com/felipe3103

---
+++ script.js
@@ -0,0 +1,21 @@
+//Autor: Felipe do Nascimento Fernandes
+//https://github.com/felipe3103
+//exercicio 1
+
+//indefinida
+
+
+let nome;
+console.log(nome)
+
+
+//vazia
+
+let disciplina = null;
+console.log(disciplina)
+
+
+//com valor
+
+let aluno = "Aluninho";
+console.log(aluno)
