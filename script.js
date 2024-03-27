fbd7df638ea943325c8f902603194d1229c37f6b
Felipe <felipenafe@gmail.com>
2024-03-27 13:05:57 -0300

    Felipe Fernandes https://github.com/felipe3103

--- script.js
+++ script.js
@@ -54,3 +54,35 @@ else {
     document.write("false")
 }
 document.write("<br>")
+
+//exercicio 3 
+
+function calcularIMC(peso, altura) {
+    return peso / (altura * altura);
+}
+
+function classificarIMC(imc) {
+    if (imc < 18.5) {
+        return "abaixo do peso";
+    } else if (imc >= 18.5 && imc <= 24.9) {
+        return "no peso ideal";
+    } else {
+        return "acima do peso";
+    }
+}
+
+function main() {
+    // Obter peso e altura do usuário (em kg e metros, respectivamente)
+    const peso = parseFloat(prompt("Digite seu peso em kg:"));
+    const altura = parseFloat(prompt("Digite sua altura em metros:"));
+
+    // Calcular IMC
+    const imc = calcularIMC(peso, altura);
+
+    // Classificar IMC e exibir mensagem
+    const classificacao = classificarIMC(imc);
+    console.log(`Seu IMC é ${imc.toFixed(2)} - Você está ${classificacao}.`);
+}
+
+// Executar o programa
+main();
