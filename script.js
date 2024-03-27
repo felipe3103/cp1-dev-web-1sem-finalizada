bfac35b2acfd58459cfaade5ec9c478245b8e5c2
Felipe <felipenafe@gmail.com>
2024-03-27 13:05:13 -0300

    Felipe Fernandes https://github.com/felipe3103

--- script.js
+++ script.js
@@ -19,3 +19,38 @@ console.log(disciplina)
 
 let aluno = "Aluninho";
 console.log(aluno)
+
+//exercicio 2
+/*
+== (igualdade)
+!=  (desiguldade)
+>= (maior ou igual)
+*/
+//==
+
+if (8 == 8) {
+    document.write("true")
+}
+else {
+    document.write("false")
+}
+document.write("<br>")
+//!=
+
+if (2 != 3) {
+    document.write("true")
+}
+else {
+    document.write("false")
+}
+document.write("<br>")
+
+//>=
+
+if (9 >= 8) {
+    document.write("true")
+}
+else {
+    document.write("false")
+}
+document.write("<br>")
